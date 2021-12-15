1c3165b026b56bce3cdb235ede8a9345dd04f346
santi-rivas <72987767+santi-rivas@users.noreply.github.com>
2021-12-15 06:53:55 +0100

    Primer commit

---
+++ GITTEST_1512/src/gittest_1512/GITTEST_1512.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest_1512;
+
+/**
+ *
+ * @author santi
+ */
+public class GITTEST_1512 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

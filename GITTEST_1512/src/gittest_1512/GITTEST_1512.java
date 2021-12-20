0031db42455e6342992c158e3d7e797c046adaf2
santi-rivas <72987767+santi-rivas@users.noreply.github.com>
2021-12-20 14:07:40 +0100

    Primer commit

--- GITTEST_1512/src/gittest_1512/GITTEST_1512.java
+++ GITTEST_1512/src/gittest_1512/GITTEST_1512.java
@@ -16,6 +16,8 @@ public class GITTEST_1512 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Prueba GIT");
     }
     
 }
